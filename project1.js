750e378fddc0298ab7f2d8e436c190b222f0885d
shushghushchyan <145218409+shushghushchyan@users.noreply.github.com>
2023-10-18 13:20:06 +0400

    products.css...

--- project1.js
+++ project1.js
@@ -1,12 +1,138 @@
+const productData = [
+    {
+        title: "I'm a product",
+        price: 15.00,
+        tag: ["home", "women"],
+        url: "https://static.wixstatic.com/media/c837a6_944cb31b5c7d4659b7a0c6612b4df573~mv2.jpg/v1/fill/w_220,h_293,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/c837a6_944cb31b5c7d4659b7a0c6612b4df573~mv2.jpg",
+        
+    },
+    {
+        title: "I'm a product",
+        price: 15.00,
+        tag:["home","men"],
+        url: "https://static.wixstatic.com/media/c837a6_fd300737ecf3406b83fd9be5d67463e4~mv2.jpg/v1/fill/w_220,h_293,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/c837a6_fd300737ecf3406b83fd9be5d67463e4~mv2.jpg",
+        
+    },
+    {
+        title: "I'm a product",
+        price: 15.00,
+        tag: ["home","men"],
+        url: "https://static.wixstatic.com/media/c837a6_6a83893e75e048b3a61c7a8711b93da3~mv2.jpg/v1/fill/w_220,h_293,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/c837a6_6a83893e75e048b3a61c7a8711b93da3~mv2.jpg",
+        
+    },
+    {
+        title: "I'm a product",
+        price: 15.00,
+        tag: ["home","men"],
+        url: "https://static.wixstatic.com/media/c837a6_0ab9ff5bbc6f41a5b2dffae389cfb2b7~mv2.jpg/v1/fill/w_220,h_293,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/c837a6_0ab9ff5bbc6f41a5b2dffae389cfb2b7~mv2.jpg",
+        
+    },
+    {
+        title: "I'm a product",
+        price: 15.00,
+        tag: ["home",],
+        url: "https://static.wixstatic.com/media/c837a6_f8d49ec34c8a4f95890362cc7a53f89b~mv2.jpg/v1/fill/w_220,h_293,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/c837a6_f8d49ec34c8a4f95890362cc7a53f89b~mv2.jpg",
+        
+    },
+    {
+        title: "I'm a product",
+        price: 15.00,
+        tag: ["home","women"],
+        url: "https://static.wixstatic.com/media/c837a6_2308b08ad22c4e4aa919906536a1b5d1~mv2.jpg/v1/fill/w_220,h_293,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/c837a6_2308b08ad22c4e4aa919906536a1b5d1~mv2.jpg",
+        
+    },
+    {
+        title: "I'm a product",
+        price: 15.00,
+        tag: ["home","men"],
+        url: "https://static.wixstatic.com/media/c837a6_8ea5cc0a06664ef6a3c1cf64e1cb5a27~mv2.jpg/v1/fill/w_220,h_293,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/c837a6_8ea5cc0a06664ef6a3c1cf64e1cb5a27~mv2.jpg",
+        
+    },
+    {
+        title: "I'm a product",
+        price: 15.00,
+        tag: ["home","women",],
+        url: "https://static.wixstatic.com/media/c837a6_ce32dd298b274ba280efa267e075be21~mv2.jpg/v1/fill/w_220,h_293,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/c837a6_ce32dd298b274ba280efa267e075be21~mv2.jpg",
+    },
+    {
+        title: "I'm a product",
+        price: 15.00,
+        tag: ["women"],
+        url: "https://static.wixstatic.com/media/c837a6_7d2a4773c9714fcd86ed78a0b9d1d086~mv2.jpg/v1/fill/w_220,h_293,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/c837a6_7d2a4773c9714fcd86ed78a0b9d1d086~mv2.jpg",
+        url2:"https://static.wixstatic.com/media/c837a6_34b176206c8842c6981aacc7d8277496~mv2.jpg/v1/fill/w_220,h_293,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/c837a6_34b176206c8842c6981aacc7d8277496~mv2.jpg"
+
+    },
+    {
+        title: "I'm a product",
+        price: 15.00,
+        tag: ["women"],
+        url: "https://static.wixstatic.com/media/c837a6_8558a07ad8084a9cb9de58022d440527~mv2.jpg/v1/fill/w_220,h_293,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/c837a6_8558a07ad8084a9cb9de58022d440527~mv2.jpg",
+        url2:"https://static.wixstatic.com/media/c837a6_b62932d93c7e403e8be83147da036347~mv2.jpg/v1/fill/w_220,h_293,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/c837a6_b62932d93c7e403e8be83147da036347~mv2.jpg",
+
+    },
+    {
+        title: "I'm a product",
+        price: 15.00,
+        tag: ["women"],
+        url:"https://static.wixstatic.com/media/c837a6_dff384ebdb954fd9a5903b028ce213c7~mv2.jpg/v1/fill/w_220,h_293,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/c837a6_dff384ebdb954fd9a5903b028ce213c7~mv2.jpg",
+    },
+    {
+        title: "I'm a product",
+        price: 15.00,
+        tag: ["women"],
+        url:"https://static.wixstatic.com/media/c837a6_a38998fd0bcd476d865747e88d09a971~mv2.jpg/v1/fill/w_220,h_293,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/c837a6_a38998fd0bcd476d865747e88d09a971~mv2.jpg",
+        url2:"https://static.wixstatic.com/media/c837a6_e57f006d920347efa9e390762ecb7120~mv2.jpg/v1/fill/w_220,h_293,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/c837a6_e57f006d920347efa9e390762ecb7120~mv2.jpg",
+    },
+    {
+        title: "I'm a product",
+        price: 15.00,
+        tag:["women"],
+        url:"https://static.wixstatic.com/media/c837a6_2308b08ad22c4e4aa919906536a1b5d1~mv2.jpg/v1/fill/w_220,h_293,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/c837a6_2308b08ad22c4e4aa919906536a1b5d1~mv2.jpg",
+    },
+    {
+        title: "I'm a product",
+        price: 15.00,
+        tag: ["men"],
+        url:"https://static.wixstatic.com/media/c837a6_09a2ce6147674cda8a158a7529e85afc~mv2.jpg/v1/fill/w_220,h_293,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/c837a6_09a2ce6147674cda8a158a7529e85afc~mv2.jpg",
+        url2:"https://static.wixstatic.com/media/c837a6_22d6a5b4c57d437c8f2097ab1e29f97f~mv2.jpg/v1/fill/w_220,h_293,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/c837a6_22d6a5b4c57d437c8f2097ab1e29f97f~mv2.jpg",
+    }
+,
+{
+    title: "I'm a product",
+    price: 15.00,
+    tag: ["men"],
+    url:"https://static.wixstatic.com/media/c837a6_b595cfde79d140ffa9687f4ed9391f75~mv2.jpg/v1/fill/w_220,h_293,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/c837a6_b595cfde79d140ffa9687f4ed9391f75~mv2.jpg",
+    url2:"https://static.wixstatic.com/media/c837a6_fdb039110b784858933d038f14f78b1c~mv2.jpg/v1/fill/w_220,h_293,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/c837a6_fdb039110b784858933d038f14f78b1c~mv2.jpg",
+},
+{
+    title: "I'm a product",
+    price: 15.00,
+    tag: ["men"],
+    url:"https://static.wixstatic.com/media/c837a6_e7a5f8c65fb8477d8160755b217935a4~mv2.jpg/v1/fill/w_220,h_293,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/c837a6_e7a5f8c65fb8477d8160755b217935a4~mv2.jpg",
+    url2:"https://static.wixstatic.com/media/c837a6_8645b4599db54d359dfe8bb70471e801~mv2.jpg/v1/fill/w_220,h_293,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/c837a6_8645b4599db54d359dfe8bb70471e801~mv2.jpg",
+
+},
+{
+    title: "I'm a product",
+    price: 15.00,
+    tag: ["men"],
+    url:"https://static.wixstatic.com/media/c837a6_9359210da13b4ad5b6c62faf5c369b6b~mv2.jpg/v1/fill/w_220,h_293,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/c837a6_9359210da13b4ad5b6c62faf5c369b6b~mv2.jpg",
+}
+
+]
 AOS.init();
 
-window.onload = function() {
-    setTimeout(function() {
-        const ThereIsOneforEveryone = document.getElementById("nerqevidiv");
-        ThereIsOneforEveryone.style.display = "block";
-        
-    }, 1000); 
-};
+const ThereIsOneforEveryone = document.getElementById("nerqevidiv");
+
+if (ThereIsOneforEveryone) {
+    window.onload = function() {
+        setTimeout(function() {
+            ThereIsOneforEveryone.style.display = "block";
+            
+        }, 1000); 
+    };
+    
+}
 
 window.addEventListener ('scroll', function()   {
 const element = document.getElementById("getoff");
@@ -17,27 +143,59 @@ if(elementposition<screenposition){
     slaqdzakh.style.animation = 'slide 2s linear';
     slaqaj.style.animation = 'slide-right 2s linear';
 }
-    else{
-        slaqdzakh.style.animation = 'paused'  
-        slaqaj.style.animation = 'paused'  }
+else{
+    slaqdzakh.style.animation = 'paused'  
+    slaqaj.style.animation = 'paused'  }
 }
-
-
 )
 
+const fragmentIdentifier = window.location.hash.slice(1); 
 
-
-// const showme = document.getElementById("shopwomen")
-// showme.addEventListener("mouseover", function() {
-//     const hiddentitle = document.getElementById("hiddentitle");
-
-//     hiddentitle.style.display = "block";
-// });
-// showme.addEventListener("mouseout", function() {
-//     hiddentitle.style.display = "none";
-// });
+console.log(fragmentIdentifier);
 
 
 
+const displayProduct = (filter) => {
+    productData.filter((el) => el.tag.includes(filter)).forEach((item, index) => {
+        const products = document.getElementById("products");
+    
+        const product = document.createElement("div");
+        product.setAttribute("id", "productid" + index);
+        product.className = "productclass";
+    
+        const divForImage = document.createElement("div");
+        divForImage.setAttribute("id", "divforImage" + index);
+    
+        const imgElement = document.createElement("img");
+        imgElement.src = item.url;
 
+        if (item.url2) {
+            imgElement.addEventListener("mouseenter", () => imgElement.src = item.url2);
+            
+            imgElement.addEventListener("mouseleave", () => imgElement.src = item.url);
+        }      
+
+        const boxfortitlesndprice = document.createElement("div")
+        boxfortitlesndprice.setAttribute("id","boxfortitlesndprice")
+
+
+
+        const title = document.createElement("div");
+        title.setAttribute("id", "titleproduct");
+        title.innerHTML = productData[0].title;
+        const price = document.createElement("div");
+        price.setAttribute("id","priceproduct");
+        price.innerHTML = productData[0].price + "$";
+
+        boxfortitlesndprice.appendChild(title);
+        boxfortitlesndprice.appendChild(price);
+        divForImage.appendChild(imgElement);
+        product.appendChild(divForImage);
+        product.appendChild(boxfortitlesndprice);
+        products.appendChild(product);
+        
+    })
+}
+
+displayProduct(fragmentIdentifier === "" ? 'home' : fragmentIdentifier)
 
