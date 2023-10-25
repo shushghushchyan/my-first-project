58d58f4dd812eb5418b01692f6233a722a05edcc
shushghushchyan <145218409+shushghushchyan@users.noreply.github.com>
2023-10-25 12:27:23 +0400

    hshs

--- project1.js
+++ project1.js
@@ -1,126 +1,195 @@
 const productData = [
     {
+        id: 1,
         title: "I'm a product",
-        price: 15.00,
-        tag: ["home", "women"],
-        url: "https://static.wixstatic.com/media/c837a6_944cb31b5c7d4659b7a0c6612b4df573~mv2.jpg/v1/fill/w_220,h_293,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/c837a6_944cb31b5c7d4659b7a0c6612b4df573~mv2.jpg",
-        
-    },
-    {
-        title: "I'm a product",
-        price: 15.00,
-        tag:["home","men"],
-        url: "https://static.wixstatic.com/media/c837a6_fd300737ecf3406b83fd9be5d67463e4~mv2.jpg/v1/fill/w_220,h_293,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/c837a6_fd300737ecf3406b83fd9be5d67463e4~mv2.jpg",
-        
-    },
-    {
-        title: "I'm a product",
-        price: 15.00,
-        tag: ["home","men"],
-        url: "https://static.wixstatic.com/media/c837a6_6a83893e75e048b3a61c7a8711b93da3~mv2.jpg/v1/fill/w_220,h_293,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/c837a6_6a83893e75e048b3a61c7a8711b93da3~mv2.jpg",
-        
-    },
-    {
-        title: "I'm a product",
-        price: 15.00,
-        tag: ["home","men"],
-        url: "https://static.wixstatic.com/media/c837a6_0ab9ff5bbc6f41a5b2dffae389cfb2b7~mv2.jpg/v1/fill/w_220,h_293,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/c837a6_0ab9ff5bbc6f41a5b2dffae389cfb2b7~mv2.jpg",
-        
-    },
-    {
-        title: "I'm a product",
-        price: 15.00,
-        tag: ["home",],
-        url: "https://static.wixstatic.com/media/c837a6_f8d49ec34c8a4f95890362cc7a53f89b~mv2.jpg/v1/fill/w_220,h_293,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/c837a6_f8d49ec34c8a4f95890362cc7a53f89b~mv2.jpg",
-        
-    },
-    {
-        title: "I'm a product",
-        price: 15.00,
-        tag: ["home","women"],
-        url: "https://static.wixstatic.com/media/c837a6_2308b08ad22c4e4aa919906536a1b5d1~mv2.jpg/v1/fill/w_220,h_293,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/c837a6_2308b08ad22c4e4aa919906536a1b5d1~mv2.jpg",
-        
-    },
-    {
-        title: "I'm a product",
-        price: 15.00,
-        tag: ["home","men"],
-        url: "https://static.wixstatic.com/media/c837a6_8ea5cc0a06664ef6a3c1cf64e1cb5a27~mv2.jpg/v1/fill/w_220,h_293,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/c837a6_8ea5cc0a06664ef6a3c1cf64e1cb5a27~mv2.jpg",
-        
-    },
-    {
-        title: "I'm a product",
-        price: 15.00,
-        tag: ["home","women",],
-        url: "https://static.wixstatic.com/media/c837a6_ce32dd298b274ba280efa267e075be21~mv2.jpg/v1/fill/w_220,h_293,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/c837a6_ce32dd298b274ba280efa267e075be21~mv2.jpg",
-    },
-    {
-        title: "I'm a product",
-        price: 15.00,
-        tag: ["women"],
-        url: "https://static.wixstatic.com/media/c837a6_7d2a4773c9714fcd86ed78a0b9d1d086~mv2.jpg/v1/fill/w_220,h_293,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/c837a6_7d2a4773c9714fcd86ed78a0b9d1d086~mv2.jpg",
-        url2:"https://static.wixstatic.com/media/c837a6_34b176206c8842c6981aacc7d8277496~mv2.jpg/v1/fill/w_220,h_293,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/c837a6_34b176206c8842c6981aacc7d8277496~mv2.jpg"
-
-    },
-    {
-        title: "I'm a product",
-        price: 15.00,
-        tag: ["women"],
-        url: "https://static.wixstatic.com/media/c837a6_8558a07ad8084a9cb9de58022d440527~mv2.jpg/v1/fill/w_220,h_293,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/c837a6_8558a07ad8084a9cb9de58022d440527~mv2.jpg",
-        url2:"https://static.wixstatic.com/media/c837a6_b62932d93c7e403e8be83147da036347~mv2.jpg/v1/fill/w_220,h_293,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/c837a6_b62932d93c7e403e8be83147da036347~mv2.jpg",
-
-    },
-    {
-        title: "I'm a product",
-        price: 15.00,
-        tag: ["women"],
-        url:"https://static.wixstatic.com/media/c837a6_dff384ebdb954fd9a5903b028ce213c7~mv2.jpg/v1/fill/w_220,h_293,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/c837a6_dff384ebdb954fd9a5903b028ce213c7~mv2.jpg",
-    },
-    {
-        title: "I'm a product",
-        price: 15.00,
-        tag: ["women"],
-        url:"https://static.wixstatic.com/media/c837a6_a38998fd0bcd476d865747e88d09a971~mv2.jpg/v1/fill/w_220,h_293,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/c837a6_a38998fd0bcd476d865747e88d09a971~mv2.jpg",
-        url2:"https://static.wixstatic.com/media/c837a6_e57f006d920347efa9e390762ecb7120~mv2.jpg/v1/fill/w_220,h_293,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/c837a6_e57f006d920347efa9e390762ecb7120~mv2.jpg",
-    },
-    {
-        title: "I'm a product",
-        price: 15.00,
-        tag:["women"],
-        url:"https://static.wixstatic.com/media/c837a6_2308b08ad22c4e4aa919906536a1b5d1~mv2.jpg/v1/fill/w_220,h_293,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/c837a6_2308b08ad22c4e4aa919906536a1b5d1~mv2.jpg",
-    },
-    {
-        title: "I'm a product",
-        price: 15.00,
+        price: 16.00,
+        info:"Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.",
+        descript:"Lorem ipsum dolor sit amet.",
+        sku:"0001",
         tag: ["men"],
-        url:"https://static.wixstatic.com/media/c837a6_09a2ce6147674cda8a158a7529e85afc~mv2.jpg/v1/fill/w_220,h_293,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/c837a6_09a2ce6147674cda8a158a7529e85afc~mv2.jpg",
-        url2:"https://static.wixstatic.com/media/c837a6_22d6a5b4c57d437c8f2097ab1e29f97f~mv2.jpg/v1/fill/w_220,h_293,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/c837a6_22d6a5b4c57d437c8f2097ab1e29f97f~mv2.jpg",
+        url:"https://static.wixstatic.com/media/c837a6_944cb31b5c7d4659b7a0c6612b4df573~mv2.jpg",
     }
-,
-{
-    title: "I'm a product",
-    price: 15.00,
-    tag: ["men"],
-    url:"https://static.wixstatic.com/media/c837a6_b595cfde79d140ffa9687f4ed9391f75~mv2.jpg/v1/fill/w_220,h_293,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/c837a6_b595cfde79d140ffa9687f4ed9391f75~mv2.jpg",
-    url2:"https://static.wixstatic.com/media/c837a6_fdb039110b784858933d038f14f78b1c~mv2.jpg/v1/fill/w_220,h_293,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/c837a6_fdb039110b784858933d038f14f78b1c~mv2.jpg",
-},
-{
-    title: "I'm a product",
-    price: 15.00,
-    tag: ["men"],
-    url:"https://static.wixstatic.com/media/c837a6_e7a5f8c65fb8477d8160755b217935a4~mv2.jpg/v1/fill/w_220,h_293,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/c837a6_e7a5f8c65fb8477d8160755b217935a4~mv2.jpg",
-    url2:"https://static.wixstatic.com/media/c837a6_8645b4599db54d359dfe8bb70471e801~mv2.jpg/v1/fill/w_220,h_293,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/c837a6_8645b4599db54d359dfe8bb70471e801~mv2.jpg",
+    ,
+    {
+        id: 2,
+        title: "I'm a product",
+        price: 19.00,
+        info:"Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.",
+        descript:"Lorem ipsum dolor sit amet.",
+        sku:"0002",
+        tag: ["home", "women"],
+        url: "https://static.wixstatic.com/media/c837a6_944cb31b5c7d4659b7a0c6612b4df573~mv2.jpg",
+        
+    },
+    {
+        id: 3,
+        title: "I'm a product",
+        price: 20.00,
+        info:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, ullam..",
+        descript:"Lorem ipsum dolor sit amet.",
+        sku:"0003",
+        tag:["home","men"],
+        url: "https://static.wixstatic.com/media/c837a6_fd300737ecf3406b83fd9be5d67463e4~mv2.jpg",
+        
+    },
+    {
+        id: 4,
+        title: "I'm a product",
+        price: 30.00,
+         info:"Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.",
+        descript:"Lorem ipsum dolor sit amet.",
+        sku:"0004",
+        tag: ["home","men"],
+        url: "https://static.wixstatic.com/media/c837a6_6a83893e75e048b3a61c7a8711b93da3~mv2.jpg",
+        
+    },
+    {
+        id: 5,
+        title: "I'm a product",
+        price: 23.00,
+         info:"Lorem ipsum dolor sit amet.Lorem ipsum dolor sit ametLorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, ullam..",
+        descript:"Lorem ipsum dolor sit amet.",
+        sku:"0005",
+        tag: ["men"],
+        url:"https://static.wixstatic.com/media/c837a6_fdb039110b784858933d038f14f78b1c~mv2.jpg",
+        url2:"https://static.wixstatic.com/media/c837a6_b595cfde79d140ffa9687f4ed9391f75~mv2.jpg",
+    },
+    {
+        id: 6,
+        title: "I'm a product",
+        price: 10.00,
+        info:"Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.",
+        descript:"Lorem ipsum dolor sit ametLorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, ullam..",
+        sku:"0006",
+        tag: ["home","men"],
+        url: "https://static.wixstatic.com/media/c837a6_0ab9ff5bbc6f41a5b2dffae389cfb2b7~mv2.jpg",
+        
+    },
+    {
+        id: 7,
+        title: "I'm a product",
+        price: 28.00,
+        info:"Lorem ipsum dolor sit amet.Lorem ipsum dolor sit ametLorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, ullam..",
+        descript:"Lorem ipsum dolor sit amet.",
+        sku:"0007",
+        tag: ["home",],
+        url: "https://static.wixstatic.com/media/c837a6_f8d49ec34c8a4f95890362cc7a53f89b~mv2.jpg",
+        
+    },
+    {
+        id: 8,
+        title: "I'm a product",
+        price: 39.00,
+         info:"Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.",
+        descript:"Lorem ipsum dolor sit ametLorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, ullam..",
+        sku:"0008",
+        tag: ["home","women"],
+        url: "https://static.wixstatic.com/media/c837a6_2308b08ad22c4e4aa919906536a1b5d1~mv2.jpg",
+        
+    },
+    {
+        id: 9,
+        title: "I'm a product",
+        price: 37.00,
+         info:"Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.",
+        descript:"Lorem ipsum dolor sit amet.",
+        sku:"0009",
+        tag: ["men"],
+        url:"https://static.wixstatic.com/media/c837a6_8645b4599db54d359dfe8bb70471e801~mv2.jpg",
+        url2:"https://static.wixstatic.com/media/c837a6_e7a5f8c65fb8477d8160755b217935a4~mv2.jpg",
+    
+    },
+    {   
+        id: 10,
+        title: "I'm a product",
+        price: 9.00,
+         info:"Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.",
+        descript:"Lorem ipsum dolor sit ametLorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, ullam..",
+        sku:"0010",
+        tag: ["home","men"],
+        url: "https://static.wixstatic.com/media/c837a6_8ea5cc0a06664ef6a3c1cf64e1cb5a27~mv2.jpg",
+        
+    },
+    {
+        id: 11,
+        title: "I'm a product",
+        price: 3.00,
+         nfo:"Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.",
+        descript:"Lorem ipsum dolor sit amet.",
+        sku:"0011",
+        tag: ["home","women",],
+        url: "https://static.wixstatic.com/media/c837a6_ce32dd298b274ba280efa267e075be21~mv2.jpg",
+    },
+    {
+        id: 12,
+        title: "I'm a product",
+        price: 28.00,
+        info:"Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.",
+        descript:"Lorem ipsum dolor sit amet.",
+        sku:"0012",
+        tag: ["women"],
+        url: "https://static.wixstatic.com/media/c837a6_7d2a4773c9714fcd86ed78a0b9d1d086~mv2.jpg",
+        url2:"https://static.wixstatic.com/media/c837a6_34b176206c8842c6981aacc7d8277496~mv2.jpg",
 
-},
+    },
+    {
+        id: 13,
+        title: "I'm a product",
+        price: 35.00,
+        info:"Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.",
+        descript:"Lorem ipsum dolor sit amet.",
+        sku:"0013",
+        tag: ["women"],
+        url: "https://static.wixstatic.com/media/c837a6_8558a07ad8084a9cb9de58022d440527~mv2.jpg",
+        url2:"https://static.wixstatic.com/media/c837a6_b62932d93c7e403e8be83147da036347~mv2.jpg",
+
+    },
+    {
+        id: 14,
+        title: "I'm a product",
+        price: 80.00,
+         info:"Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.",
+        descript:"Lorem ipsum dolor sit amet.",
+        sku:"0014",
+        tag: ["women"],
+        url:"https://static.wixstatic.com/media/c837a6_dff384ebdb954fd9a5903b028ce213c7~mv2.jpg",
+    },
+    {
+        id: 15,
+        title: "I'm a product",
+        price: 29.00,
+         info:"Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.",
+        descript:"Lorem ipsum dolor sit amet.",
+        sku:"0015",
+        tag: ["women"],
+        url:"https://static.wixstatic.com/media/c837a6_a38998fd0bcd476d865747e88d09a971~mv2.jpg",
+        url2:"https://static.wixstatic.com/media/c837a6_e57f006d920347efa9e390762ecb7120~mv2.jpg",
+    },
+    {
+        id: 15,
+        title: "I'm a product",
+        price: 30.00,
+        info:"Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.",
+        descript:"Lorem ipsum dolor sit amet.",
+        sku:"0016",
+        tag:["women"],
+        url:"https://static.wixstatic.com/media/c837a6_2308b08ad22c4e4aa919906536a1b5d1~mv2.jpg",
+    },
+
 {
+    id: 16,
     title: "I'm a product",
-    price: 15.00,
+    price: 29.00,
+    info:"Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.",
+    descript:"Lorem ipsum dolor sit amet.",
+    sku:"0017",
     tag: ["men"],
-    url:"https://static.wixstatic.com/media/c837a6_9359210da13b4ad5b6c62faf5c369b6b~mv2.jpg/v1/fill/w_220,h_293,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/c837a6_9359210da13b4ad5b6c62faf5c369b6b~mv2.jpg",
+    url:"https://static.wixstatic.com/media/c837a6_9359210da13b4ad5b6c62faf5c369b6b~mv2.jpg",
 }
 
 ]
 AOS.init();
+
 
 const ThereIsOneforEveryone = document.getElementById("nerqevidiv");
 
@@ -134,8 +203,11 @@ if (ThereIsOneforEveryone) {
     
 }
 
+const element = document.getElementById("getoff") || null;
+
+if(element !==null){
+
 window.addEventListener ('scroll', function()   {
-const element = document.getElementById("getoff");
 const elementposition = element.getBoundingClientRect().top;
 const screenposition = window.innerHeight;
 
@@ -149,6 +221,9 @@ else{
 }
 )
 
+}
+
+
 const fragmentIdentifier = window.location.hash.slice(1); 
 
 console.log(fragmentIdentifier);
@@ -157,9 +232,13 @@ console.log(fragmentIdentifier);
 
 const displayProduct = (filter) => {
     productData.filter((el) => el.tag.includes(filter)).forEach((item, index) => {
-        const products = document.getElementById("products");
-    
+        
+        const products = document.getElementById("products") || null;
         const product = document.createElement("div");
+        const link = document.createElement("a");
+        link.appendChild(product);
+        link.setAttribute("href", `iamaproduct.html#${item.id}`);
+
         product.setAttribute("id", "productid" + index);
         product.className = "productclass";
     
@@ -167,13 +246,16 @@ const displayProduct = (filter) => {
         divForImage.setAttribute("id", "divforImage" + index);
     
         const imgElement = document.createElement("img");
+        imgElement.setAttribute("id","img1")
         imgElement.src = item.url;
-
+       
         if (item.url2) {
             imgElement.addEventListener("mouseenter", () => imgElement.src = item.url2);
             
             imgElement.addEventListener("mouseleave", () => imgElement.src = item.url);
         }      
+
+       
 
         const boxfortitlesndprice = document.createElement("div")
         boxfortitlesndprice.setAttribute("id","boxfortitlesndprice")
@@ -182,20 +264,63 @@ const displayProduct = (filter) => {
 
         const title = document.createElement("div");
         title.setAttribute("id", "titleproduct");
-        title.innerHTML = productData[0].title;
+        title.innerHTML = item.title;
         const price = document.createElement("div");
         price.setAttribute("id","priceproduct");
-        price.innerHTML = productData[0].price + "$";
+        price.innerHTML = item.price + "$";
 
         boxfortitlesndprice.appendChild(title);
         boxfortitlesndprice.appendChild(price);
         divForImage.appendChild(imgElement);
         product.appendChild(divForImage);
         product.appendChild(boxfortitlesndprice);
-        products.appendChild(product);
+        products.appendChild(link);
         
     })
 }
 
 displayProduct(fragmentIdentifier === "" ? 'home' : fragmentIdentifier)
+
+const iamproductimg = document.getElementById("img")
+
+const displayOneProduct = (filter) => {
+    const img = document.getElementById('img')
+    img.style.background = `url(${productData[filter - 1].url})`
+    img.style.backgroundSize = 'cover'; // Set background size to cover
+    console.log(productData[filter - 1]);
+
+    const sku = document.getElementById("sku");
+    const price = document.getElementById("price");
+    const info = document.getElementById("productdetail")
+    const descript = document.getElementById("description")
+    
+    sku.innerHTML = `SKU: ${productData[filter - 1].sku}`
+    price.innerHTML = `PRICE: ${productData[filter-1].price}$`
+    info.innerHTML = `${productData[filter-1].info}`
+    descript.innerHTML = `${productData[filter-1].descript}`
+
+}
+
+
+// productData[fragmentIdentifier - 1]
+
+displayOneProduct(fragmentIdentifier)
+
+
+const addToCart = document.getElementById("addtocart");
+const cartSideBar = document.getElementById("cartSidebar");
+const transparent = document.getElementById("transparent");
+
+addToCart.addEventListener("click", () => {
+    cartSideBar.classList.remove("hidden");
+    cartSideBar.style.right = "0";
+
+
+});
+const closeCartSidebar = document.getElementById("closecartSidebar");
+closeCartSidebar.addEventListener("click", () => {
+    cartSideBar.classList= "hidden";
+    cartSideBar.style.right = "-400px";
+
+});
 
